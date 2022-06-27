b1c9066612299f09db735ad2a328c5a3dfa2f49f
ro-ze1106 <roze56477@gmail.com>
2022-06-27 22:38:15 +0900

    test-2

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,8 +7,12 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    '@storybook/addon-a11y'
   ],
+  features: {
+    interactionsDebugger: true, // 👈 Enable playback controls
+  },
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
